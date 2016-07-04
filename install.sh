05fb5b3ca63fa4320e85594bed3b1da74c72408d
1egoman <rsg1egoman@gmail.com>
2016-07-04 19:09:34 -0400

    add symlink install script

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# symlink the dotfile from its current location to ~/.dotfile
+function symlink {
+  ln -s $PWD/$1 $HOME/.$1
+  echo "Installed $1 in $HOME/.$1"
+}
+
+for filename in *; do
+  # not this script
+  if [[ "$filename" != "install.sh" ]] &&
+     [[ "$filename" != "LICENCE" ]] &&
+     [[ "$filename" != "README.md" ]] &&
+     [[ "$filename" != "."* ]]; then
+
+    # should we delete an old version of the dotfile?
+    if [[ -f "$HOME/.$filename" ]]; then
+      read -p "Dotfile .$filename already exists. Should we replace it? " yn
+      case $yn in
+        [yY]* )
+          rm $HOME/.$filename; # delete the old dotfile
+          symlink $filename; # simlink the new dotfile
+          ;;
+        * )
+          echo "Skipping $filename..."
+          ;;
+      esac
+    else
+      # add the new file without question
+      symlink $filename
+    fi
+  fi
+done
